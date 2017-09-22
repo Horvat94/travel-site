2413522dda3167d1f8ff9a372a2723d0166ef80b
Tadej <horvat.tadej@hotmail.com>
2017-09-22 12:41:11 +0200

    dokončan large-hero block in intro vsebina block

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),//spremenljivka gulp
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssVars = require('postcss-simple-vars'),
+	nested = require('postcss-nested'),// spremenljivki watch dodamo zahtevek za watch
+	cssImport = require('postcss-import'),
+	mixins = require('postcss-mixins');
+
+gulp.task('styles', function(){//ko se shrani gledano datoteko prekopira v novo datoteko..
+	//gulp.src()- kje črpati vodo , gulp.dest()-kam hočemo postaviti vodo, pipe()
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssImport,mixins,nested,cssVars,autoprefixer]))
+	.on('error', function(errorInfo){
+		console.log(errorInfo.toString());
+		this.emit('end');//če pride do kakšne napake normalno deluje naprej
+	})
+	.pipe(gulp.dest('./app/temp/styles'));//gulp.src()- je asyncrona funkcija zato uporabimo return
+	
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+	var gulp = require('gulp'),//spremenljivka gulp
+	watch = require('gulp-watch'),
+	browserSync = require('browser-sync').create();
+
+	gulp.task('watch', function(){//funkcija posluša spremembe, ki jih shranimo
+    
+    browserSync.init({//avtomatsko osveževanje strani
+    	notify: false,//skrije obvestilo v brskalniku
+        server: {//strežnik na računalniku zažene brskalnik
+            baseDir: "app"//povemo kje se nahaja naš html
+        }
+
+    });
+    
+	watch('./app/index.html',function(){
+	   browserSync.reload(); //osvveži brskalnik
+	});
+
+    
+	watch('./app/assets/styles/**/*.css',function(){
+		gulp.start('cssInject');// zažene določen gulp task.. metoda potrebuje ime narejeneega taska
+	});
+});
+
+gulp.task('cssInject',['styles'], function(){// , dependencies, dokler se ne zažene styles task se css-inject ne bo izvedel, tako se lahko style.css pravočasno ustvari in ga pipamo v browsersync
+	gulp.src("./app/temp/styles/styles.css")//vzamemo vsebino kompilanega css in jih predamo browsersync tako da lahko injecta na stran med procesom
+		.pipe(browserSync.stream())//z pipo ga pretočimo v browsersync
+		;
+});
