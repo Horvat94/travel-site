1351ad6778881a6183203fd130d5d7f3fa067186
Tadej <horvat.tadej@hotmail.com>
2017-09-25 20:06:27 +0200

    dokončan Header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ var gulp = require('gulp'),//spremenljivka gulp
 	cssVars = require('postcss-simple-vars'),
 	nested = require('postcss-nested'),// spremenljivki watch dodamo zahtevek za watch
 	cssImport = require('postcss-import'),
-	mixins = require('postcss-mixins');
+	mixins = require('postcss-mixins'),
+	hexrgba = require("postcss-hexrgba");//hex v rgba
 
 gulp.task('styles', function(){//ko se shrani gledano datoteko prekopira v novo datoteko..
 	//gulp.src()- kje črpati vodo , gulp.dest()-kam hočemo postaviti vodo, pipe()
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport,mixins,nested,cssVars,autoprefixer]))
+	.pipe(postcss([cssImport,mixins,nested,cssVars,hexrgba,autoprefixer]))
 	.on('error', function(errorInfo){
 		console.log(errorInfo.toString());
 		this.emit('end');//če pride do kakšne napake normalno deluje naprej
